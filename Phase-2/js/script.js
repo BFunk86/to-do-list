63fc0dbf5fe5b4f8e31b83550dfad5026a3437c3
Brandon Young <fultoncountycommercialcleaning@gmail.com>
2017-08-04 02:00:11 -0400

    Refactored Code for Phase 2

--- Phase-2/js/script.js
+++ Phase-2/js/script.js
@@ -1,37 +1,19 @@
-/**
- * @author: Brandon Young
- *
- */
-(function(){
+function Task(task) {
+    this.task = task;
+    this.checked = false;
+    this.buildHTML = function() {
 
-
-    var list = document.getElementById("list");
-    var input = document.getElementById("task");
-    var form = document.getElementById('newTask');
-
-    function Task(task) {
-        this.task = task;
-        this.checked = false;
-        this.uncheck = function() {
-            this.checked = false;
-        };
-
-        this.check = function() {
-            this.checked = true;
-        }
     }
+}
 
-    Task.prototype.addTask = function() {
-        list.innerHTML += "<li>" + "<input type='checkbox'>" +  this.task + "</li>";
-    };
 
-    form.addEventListener("submit", function(e) {
+$(document).ready(function() {
+    // Check for form submit
+    $("newTask").submit(function(e) {
         e.preventDefault();
-        var daTask = new Task(input.value);
-        daTask.addTask();
-        form.reset();
-    })
+        // get new task
+        var task = $("#task").val();
 
 
-
-}());
+    });
+});
